326ddbe69cbe3294c79ed057dfcf05a83829d9d3
Dharminder Singh <mr.dee.ghogha@gmail.com>
2021-04-26 20:24:39 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,313 @@
+ document.onkeydown = function(event){
+             if(event.repeat) return
+                if (event.keyCode == 83 || event.which == 83){
+                    Sa()
+                }
+            };
+            document.onkeyup = function(event){
+             if(event.repeat) return
+                if (event.keyCode == 83 || event.which == 83){
+                    ss()
+                }
+            };
+            var Saa = new Audio('audio/100_Sa_B_harmonium1_1.mp3')
+                
+            function Sa(){
+                Saa.play();
+                console.log("sa");
+                document.querySelector('#sa').style.boxShadow = '-3px -3px 6px rgba(255, 255, 255, 0.9)',
+                '3px 1px 0px rgba(0, 0, 0, 0.1)';
+                }
+            function ss(){
+                Saa.pause();
+                Saa.currentTime = 0;
+                document.querySelector('#sa').style.boxShadow = '3px 3px 5px rgba(0, 0, 0, 0.7)';
+            }    
+                
+            
+                document.querySelector('html').onkeydown = function(event){
+                if(event.repeat) return
+                if (event.keyCode == 68 || event.which == 68){
+                    re()
+                }
+                if (event.keyCode == 86 || event.which == 86){
+                    ga()
+                }
+                if (event.keyCode == 71 || event.which == 71){
+                    ma()
+                }
+                if (event.keyCode == 72 || event.which == 72){
+                    pa()
+                }
+                if (event.keyCode == 74 || event.which == 74){
+                    dha()
+                }
+                if (event.keyCode == 188 || event.which == 188){
+                    ni()
+                }
+                if (event.keyCode == 76 || event.which == 76){
+                    saa()
+                }
+                     if (event.keyCode == 90 || event.which == 90){
+                    Z()
+                }
+                  if (event.keyCode == 88 || event.which == 88){
+                    X()
+                }
+                  if (event.keyCode == 67 || event.which == 67){
+                    ABC()
+                }
+                  if (event.keyCode == 66 || event.which == 66){
+                    ABB()
+                }
+                  if (event.keyCode == 78 || event.which == 78){
+                    N()
+                }
+                  if (event.keyCode == 77 || event.which == 77){
+                    M()
+                }
+                  if (event.keyCode == 190 || event.which == 190){
+                    Q()
+                }
+            };
+            
+             document.querySelector('html').onkeyup = function(event){
+                if(event.repeat) return
+                if (event.keyCode == 68 || event.which == 68){
+                    rr()
+                }
+                if (event.keyCode == 86 || event.which == 86){
+                    gg()
+                }
+                if (event.keyCode == 71 || event.which == 71){
+                    mm()
+                }
+                if (event.keyCode == 72 || event.which == 72){
+                    pp()
+                }
+                if (event.keyCode == 74 || event.which == 74){
+                    dd()
+                }
+                if (event.keyCode == 188 || event.which == 188){
+                    nn()
+                }
+                if (event.keyCode == 76 || event.which == 76){
+                    sss()
+                }
+                  if (event.keyCode == 90 || event.which == 90){
+                    ZZ()
+                }
+                  if (event.keyCode == 88 || event.which == 88){
+                    XX()
+                }
+                  if (event.keyCode == 67 || event.which == 67){
+                    CC()
+                }
+                  if (event.keyCode == 66 || event.which == 66){
+                    BB()
+                }
+                  if (event.keyCode == 78 || event.which == 78){
+                    NN()
+                }
+                  if (event.keyCode == 77 || event.which == 77){
+                    MM()
+                }
+                  if (event.keyCode == 190 || event.which == 190){
+                    QQ()
+                }
+            };
+             var REE = new Audio('audio/100_Re_M_harmonium1_1.mp3')
+             var Gaa = new Audio('audio/100_Ga_M_harmonium1_1.mp3')
+             var Maa = new Audio('audio/100_Ma_M_harmonium1_1.mp3')
+             var paa = new Audio('audio/100_Pa_M_harmonium1_1.mp3')
+             var Daa = new Audio('audio/100_Dha_M_harmonium1_1.mp3')
+             var Nii = new Audio('audio/100_Ni_M_harmonium1_1.mp3')
+             var SSaa = new Audio('audio/100_Sa_H_harmonium1_1.mp3')
+             var A = new Audio('audio/100_Ni_B_harmonium1_1.mp3')
+             var B = new Audio('audio/100_Re_H_harmonium1_1.mp3')
+             var C = new Audio('audio/100_Ga_H_harmonium1_1.mp3')
+             var E = new Audio('audio/100_Ma_H_harmonium1_1.mp3')
+             var F = new Audio('audio/100_Pa_B_harmonium1_1.mp3')
+             var G = new Audio('audio/100_Dha_B_harmonium1_1.mp3')
+             var AA = new Audio('audio/100_Re_H_harmonium1_1.mp3')
+            function re(){
+                REE.play();
+                console.log("re");
+                document.querySelector('#re').style.boxShadow = '-3px -3px 6px rgba(255, 255, 255, 0.9)',
+                '3px 1px 0px rgba(0, 0, 0, 0.1)';
+            }
+            function rr(){
+                REE.pause();
+                REE.currentTime = 0;
+                document.querySelector('#re').style.boxShadow ='3px 3px 5px rgba(0, 0, 0, 0.7)';
+            }
+            
+             
+            function ga(){
+                
+                Gaa.play();
+                console.log("ga");
+                document.querySelector('#ga').style.boxShadow =  '-3px -3px 6px rgba(255, 255, 255, 0.9)',
+                '3px 3px 5px rgba(0, 0, 0, 0.7)';
+            }
+            function gg(){
+                Gaa.pause();
+                Gaa.currentTime = 0;
+                document.querySelector('#ga').style.boxShadow = '3px 3px 5px rgba(0, 0, 0, 0.7)';
+            }
+            
+            function ma(){
+                
+                Maa.play();
+                console.log("ma");
+                document.querySelector('#ma').style.boxShadow =  '-3px -3px 6px rgba(255, 255, 255, 0.9)',
+                '3px 3px 5px rgba(0, 0, 0, 0.7)';
+               
+            }
+            
+            function mm(){
+                Maa.pause();
+                Maa.currentTime = 0;
+                document.querySelector('#ma').style.boxShadow =  '3px 3px 5px rgba(0, 0, 0, 0.7)';
+            }
+        
+            function pa(){
+                
+                paa.play();
+                console.log("pa");
+                document.querySelector('#pa').style.boxShadow =  '-3px -3px 6px rgba(255, 255, 255, 0.9)',
+                '3px 3px 5px rgba(0, 0, 0, 0.7)';
+               
+            }
+            
+            function pp(){
+                paa.pause();
+                paa.currentTime = 0;
+                document.querySelector('#pa').style.boxShadow = '3px 3px 5px rgba(0, 0, 0, 0.7)';
+            }
+            function dha(){
+                
+                Daa.play();
+                console.log("Dha");
+                document.querySelector('#dha').style.boxShadow =  '-3px -3px 6px rgba(255, 255, 255, 0.9)',
+                '3px 3px 5px rgba(0, 0, 0, 0.7)';
+                
+            }
+            function dd(){
+                Daa.pause();
+                Daa.currentTime = 0;
+                document.querySelector('#dha').style.boxShadow = '3px 3px 5px rgba(0, 0, 0, 0.7)';
+            }
+            
+            function ni(){
+                
+                Nii.play();
+                console.log("ni");
+                document.querySelector('#ni').style.boxShadow =  '-3px -3px 6px rgba(255, 255, 255, 0.9)',
+                '3px 3px 5px rgba(0, 0, 0, 0.7)';
+               
+            }
+            function nn(){
+                Nii.pause();
+                Nii.currentTime = 0;
+                document.querySelector('#ni').style.boxShadow = '3px 3px 5px rgba(0, 0, 0, 0.7)';
+            }
+            
+            function saa(){
+                
+                SSaa.play();
+                console.log("sa");
+                document.querySelector('#SA').style.boxShadow =  '-3px -3px 6px rgba(255, 255, 255, 0.9)',
+                '3px 3px 5px rgba(0, 0, 0, 0.7)';
+               
+            }
+            function sss(){
+                SSaa.pause();
+                SSaa.currentTime = 0;
+                document.querySelector('#SA').style.boxShadow =  '3px 3px 5px rgba(0, 0, 0, 0.7)';
+            }
+            
+            function Z(){
+                A.play();
+                console.log("ni");
+                document.querySelector('#nii').style.boxShadow =  '-3px -3px 6px rgba(255, 255, 255, 0.9)',
+                '3px 3px 5px rgba(0, 0, 0, 0.7)';
+            }
+            function ZZ(){
+                A.pause();
+                A.currentTime = 0;
+                document.querySelector('#nii').style.boxShadow = '3px 3px 5px rgba(0, 0, 0, 0.7)';
+            }
+            
+            function X(){
+                B.play();
+                console.log("re");
+                document.querySelector('#x').style.boxShadow =  '-3px -3px 6px rgba(255, 255, 255, 0.9)',
+                '3px 3px 5px rgba(0, 0, 0, 0.7)';
+            }
+            function XX(){
+                B.pause();
+                B.currentTime = 0;
+                document.querySelector('#x').style.boxShadow = '3px 3px 5px rgba(0, 0, 0, 0.7)';
+            }
+            
+            function ABC(){
+                C.play();
+                console.log("re");
+                document.querySelector('#c').style.boxShadow =  '-3px -3px 6px rgba(255, 255, 255, 0.9)',
+                '3px 3px 5px rgba(0, 0, 0, 0.7)';
+            }
+            function CC(){
+                C.pause();
+                C.currentTime = 0;
+                document.querySelector('#c').style.boxShadow = '3px 3px 5px rgba(0, 0, 0, 0.7)';
+            }
+            
+            function ABB(){
+                E.play();
+                console.log("re");
+                document.querySelector('#b').style.boxShadow =  '-3px -3px 6px rgba(255, 255, 255, 0.9)',
+                '3px 3px 5px rgba(0, 0, 0, 0.7)';
+            }
+            function BB(){
+                E.pause();
+                E.currentTime = 0;
+                document.querySelector('#b').style.boxShadow =   '3px 3px 5px rgba(0, 0, 0, 0.7)';
+            }
+            
+            function N(){
+                F.play();
+                console.log("re");
+                document.querySelector('#n').style.boxShadow =  '-3px -3px 6px rgba(255, 255, 255, 0.9)',
+                '3px 3px 5px rgba(0, 0, 0, 0.7)';
+            }
+            function NN(){
+                F.pause();
+                F.currentTime = 0;
+                document.querySelector('#n').style.boxShadow =  '3px 3px 5px rgba(0, 0, 0, 0.7)';
+            }
+            
+            function M(){
+                G.play();
+                console.log("re");
+                document.querySelector('#m').style.boxShadow =  '-3px -3px 6px rgba(255, 255, 255, 0.9)',
+                '3px 3px 5px rgba(0, 0, 0, 0.7)';
+            }
+            function MM(){
+                G.pause();
+                G.currentTime = 0;
+                document.querySelector('#m').style.boxShadow =  '3px 3px 5px rgba(0, 0, 0, 0.7)';
+            }
+            
+            function Q(){
+                AA.play();
+                console.log("re");
+                document.querySelector('#q').style.boxShadow =  '-3px -3px 6px rgba(255, 255, 255, 0.9)',
+                '3px 3px 5px rgba(0, 0, 0, 0.7)';
+            }
+            function QQ(){
+                AA.pause();
+                AA.currentTime = 0;
+                document.querySelector('#q').style.boxShadow =  '3px 3px 5px rgba(0, 0, 0, 0.7)';
+            }
+        
